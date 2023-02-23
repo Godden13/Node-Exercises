3ad9659d8f0a7b86e71ca1b985ba95666736c3e7
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2023-02-23 07:06:52 +0100

    commit changes

---
+++ restapi/db.js
@@ -0,0 +1,16 @@
+// const { readFileSync } = require("fs");
+// function readUsers() {
+//   try{
+//   const data = readFileSync("./db.json") || "[]";
+//   return JSON.parse(data);
+//   }catch (e) {
+//     return [];
+//   }
+// }
+
+// function saveUsers(users = []) {
+//   try {
+//     const data = JSON.stringify(users, null, 4);
+
+//   }
+// }
